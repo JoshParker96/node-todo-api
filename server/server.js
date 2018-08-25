4557ecc381418d82ae81ba5b9de2939e2af99416
Josh Parker <jldparker@hotmail.co.uk>
2018-08-25 13:40:51 +0100

    Added PUT and DELETE apis for User and Todo models

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+// resolve all comment in APIs
+// refactor (hard coded vales, routes)
+// seperate APis into seperate routes
+// add one to many relationships (then update required APIs to return those relations)
+
+// seperate/add middleware
+// add server log
+
+// add unit tests
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -17,6 +27,7 @@ app.post('/todos', (req, res) => {
     let jsonResponse = {'success': true, 'data': doc}
     res.status(201).json(jsonResponse)
   }).catch((err) => {
+    // handle create and server errors
     let jsonResponse = {success: false, serverError: false, errors: err.errors, message: err.message}
     res.status(400).json(jsonResponse)
   })
@@ -27,6 +38,7 @@ app.post('/users', (req, res) => {
     let jsonResponse = {'success': true, 'data': doc}
     res.status(201).json(jsonResponse)
   }).catch((err) => {
+    // handle create and server errors
     let jsonResponse = {'success': false, errors: err.errors, 'message': err.message}
     res.status(400).json(jsonResponse)
   })
@@ -38,8 +50,106 @@ app.get('/users', (req, res) => {
     let status = 200
     res.status(status).json(jsonResponse)
   }).catch((err) => {
-    let jsonResponse = {'success': false, 'message': 'No users found'}
+    // handle server error
+    let jsonResponse = {'success': false, serverError: true}
     let status = 404
     res.status(status).json(jsonResponse)
+  })
+})
+
+app.get('/todos', (req, res) => {
+  return Todo.find().then((todos) => {
+    let jsonResponse = {'success': true, 'data': todos}
+    let status = 200
+    res.status(status).json(jsonResponse)
+  }).catch((err) => {
+    // handle server error
+    let jsonResponse = {'success': false, serverError: true}
+    let status = 404
+    res.status(status).json(jsonResponse)
+  })
+})
+
+app.put('/users/:id', (req, res) => {
+  let userId = req.params.id
+  return User.findById(userId).then((user) => {
+    // update update function to req.body
+    // return updated user
+    let userId = {id: user.id}
+    return User.update(userId, {email: req.body.email})
+  }).then(() => {
+    let jsonResponse = {'success': true}
+    let status = 200
+    res.status(status).json(jsonResponse)
+  }).catch((err) => {
+    // handle update error
+    // handle server error
+    console.log(err);
+    let jsonResponse = {'success': false, serverError: false}
+    let status = 404
+    if (err.name === 'CastError') {
+      jsonResponse['message'] = 'User does not exist'
+    }
+    res.status(status).json(jsonResponse)
+  })
+})
+
+app.put('/todos/:id', (req, res) => {
+  let todoId = req.params.id
+  return Todo.findById(todoId).then((todo) => {
+    // update update function to req.body
+    // return updated user
+    let todoId = {id: todo.id}
+    return Todo.update(todoId, {email: req.body.email})
+  }).then(() => {
+    let jsonResponse = {'success': true}
+    let status = 200
+    res.status(status).json(jsonResponse)
+  }).catch((err) => {
+    // handle update error
+    // handle server error
+    console.log(err);
+    let jsonResponse = {'success': false, serverError: false}
+    let status = 404
+    if (err.name === 'CastError') {
+      jsonResponse['message'] = 'User does not exist'
+    }
+    res.status(status).json(jsonResponse)
+  })
+})
+
+app.delete('/users/:id', (req, res) => {
+  let userId = req.params.id
+  return User.findById(userId).then((user) => {
+    // return removed user
+    return User.remove(user)
+  }).then(() => {
+    let jsonResponse = {'success': true}
+    let status = 200
+    res.status(status).json(jsonResponse)
+  }).catch((err) => {
+    console.log(err);
+    // hanlde user can not be found error
+    // hanlde delete error
+    // handle server error
+
+  })
+})
+
+app.delete('/todos/:id', (req, res) => {
+  let todoId = req.params.id
+  return Todo.findById(todoId).then((todo) => {
+    // return removed todo
+    return todo.remove(todo)
+  }).then(() => {
+    let jsonResponse = {'success': true}
+    let status = 200
+    res.status(status).json(jsonResponse)
+  }).catch((err) => {
+    console.log(err);
+    // hanlde todo can not be found error
+    // hanlde delete error
+    // handle server error
+
   })
 })
